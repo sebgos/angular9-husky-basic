1b3d11d2e2181beebc1a6bcc1c60a948f5cd931f
sebgos <gos.sebastian@gmail.com>
2020-04-02 02:48:56 +0200

    feat(feature-c): add new feature-c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { FeatureAComponent } from './feature-a/feature-a/feature-a.component'
 import { FeatureBComponent } from './feature-b/feature-b/feature-b.component'
+import { FeatureCComponent } from './feature-c/feature-c/feature-c.component'
 
 @NgModule({
-  declarations: [AppComponent, FeatureAComponent, FeatureBComponent],
+  declarations: [
+    AppComponent,
+    FeatureAComponent,
+    FeatureBComponent,
+    FeatureCComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/feature-c/feature-c/feature-c.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-feature-c',
+  templateUrl: './feature-c.component.html',
+  styleUrls: ['./feature-c.component.scss'],
+})
+export class FeatureCComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
